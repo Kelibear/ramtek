5f1c55be7e9e753f133d39bb892144f218148934
Kelibear <falihakbar18@gmail.com>
2025-02-25 19:35:23 +0700

    Update tindihtindihan.js

--- js/tindihtindihan.js
+++ js/tindihtindihan.js
@@ -1,7 +1,11 @@
-document.getElementById("heart").addEventListener("click", function() {
-    let music = document.getElementById("bg_music");
-    let letter = document.querySelector(".letter-wrapper");
+const bg_music = document.getElementById('bg_music');
 
-    music.play().catch(error => console.log("Autoplay blocked:", error));
-    letter.style.display = "block";
+function start() {
+  let bulet = document.getElementById("bulet");
+  bulet.addEventListener("transitionend", () => {
+    bg_music.play();
 });
+  bg_music.addEventListener('ended', () => {
+    bg_music.play();
+});
+}
