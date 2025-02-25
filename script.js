4ad8a4bda2cbf2e6f463f7a75413d3f162ba3009
Kelibear <falihakbar18@gmail.com>
2025-02-25 18:59:06 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("heart").addEventListener("click", function() {
-    let music = document.getElementById("bg_music");
-    let letter = document.querySelector(".letter-wrapper");
-
-    music.play().catch(error => console.log("Autoplay blocked:", error));
-    letter.style.display = "block";
-});
